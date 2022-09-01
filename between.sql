d1ecc07573a5d289e1b8841dedb7d28a29c66a8b
Pakki, Neelima <neelimapakki99@gmail.com>
2022-09-01 12:37:33 -0400

    sequel changes

--- between.sql
+++ between.sql
@@ -1,3 +1,3 @@
 SELECT column_name(s)
 FROM table_name
-WHERE column_name BETWEEN value1 AND value2;
+WHERE column_name BETWEEN value3 AND value4;
--- update.sql
+++ update.sql
@@ -1,3 +1,4 @@
 UPDATE table_name
 SET column1 = value1, column2 = value2, ...
 WHERE condition;
+you can update the column value
